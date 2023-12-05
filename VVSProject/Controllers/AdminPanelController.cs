d742376449e83728efc5e215aa831d7d478095ef
Hana Mahmutović <hmahmutovi3@etf.unsa.ba>
2023-12-05 14:38:52 +0100

    changes

--- VVSProject/Controllers/AdminPanelController.cs
+++ VVSProject/Controllers/AdminPanelController.cs
@@ -95,7 +95,7 @@ namespace SmartCafe.Controllers
             List<Drink> drinks = _context.Drinks.ToList();
             List<Drink> wantedDrinks = new List<Drink>();
 
-            int num = 1;
+            int num = 0;
             while (wantedDrinks.Count != 5)
             {
                 Ingredient ingredient = ingredients[ingredients.Count - num];
